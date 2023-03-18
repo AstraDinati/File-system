9fc836eb6a94c69850f6fff4e3820e11ef468cf6
Vadim <bigulipa@gmail.com>
2023-03-19 00:18:41 +0400

    add new less

---
+++ 9-dividing-layout-into-elements/hw1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>title</title>
+	</head>
+	<body>
+		<?php include __DIR__ .'/header.php'; ?>
+		<?php include __DIR__ .'/sidebar.php'; ?>
+		<main>
+			content
+		</main>
+		<?php include __DIR__ .'/footer.php'; ?>
+	</body>
+</html>
